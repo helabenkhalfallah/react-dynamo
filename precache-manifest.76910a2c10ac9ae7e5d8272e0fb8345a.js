1bf5b17dbb13df095360f8d36743f84bb575e0d0
helabenkhalfallah <hela@upperskills.com>
2020-04-19 13:25:44 +0200

    Updates

---
+++ precache-manifest.76910a2c10ac9ae7e5d8272e0fb8345a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0a625d774c6943a9f88ba5c4b23dc4e",
+    "url": "/react-dynamo/index.html"
+  },
+  {
+    "revision": "ebaf0d51b89f6520e73b",
+    "url": "/react-dynamo/static/css/2.a72c35d3.chunk.css"
+  },
+  {
+    "revision": "4acf89fd22c2d2ce2246",
+    "url": "/react-dynamo/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ebaf0d51b89f6520e73b",
+    "url": "/react-dynamo/static/js/2.5c5e38e3.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-dynamo/static/js/2.5c5e38e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4acf89fd22c2d2ce2246",
+    "url": "/react-dynamo/static/js/main.f1019db8.chunk.js"
+  },
+  {
+    "revision": "8b919381e012764f71b2",
+    "url": "/react-dynamo/static/js/runtime-main.427ed214.js"
+  }
+]);
